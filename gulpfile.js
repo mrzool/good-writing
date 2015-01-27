17b767f85d67b65553fe52846d058a41b218a041
zool <zool.bib@gmail.com>
2015-01-27 19:21:52 +0100

    Comment out UnCSS for the moment

--- gulpfile.js
+++ gulpfile.js
@@ -17,17 +17,15 @@ gulp.task('bs-reload', function() {
 gulp.task('styles', function() {
   return gulp.src('css/*.css')
   .pipe(concat('s.css'))
-  .pipe(uncss({
-          html: ['_site/index.html']
-          }))
   .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
   .pipe(rename({suffix: '.min'}))
   .pipe(minifycss())
+  // .pipe(uncss({ html: ['_site/index.html'] }))
   .pipe(gulp.dest('css/prod'));
 });
 
 gulp.task('default', ['browser-sync'], function() {
   gulp.watch("css/*.css", ['styles']);
-  gulp.watch("./_site/*.html", ['bs-reload']);
+  gulp.watch("./_site/*.html", ['styles', 'bs-reload']);
   gulp.watch("./_site/css/*.css", ['bs-reload']);
 });
